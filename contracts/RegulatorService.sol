52139b7a4a4bb7863b92d0d07b9f0397aaeb6b4c
Bob Remeika <bob.remeika@gmail.com>
2017-10-04 16:24:33 -0700

    Introduce proxy service to make check() logic upgradable

--- contracts/RegulatorService.sol
+++ contracts/RegulatorService.sol
@@ -1,16 +1,6 @@
 pragma solidity ^0.4.4;
 
 contract RegulatorService {
-  address public owner;
-
-  // modifier restricted() {
-  //   if (msg.sender == owner) _;
-  // }
-
-  function RegulatorService() {
-    owner = msg.sender;
-  }
-
   function check(address _participant) returns (bool) {
     return false;
   }
