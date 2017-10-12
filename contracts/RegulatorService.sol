a6846de1555651f90a3ffd72756853e03ee973bd
Bob Remeika <bob.remeika@gmail.com>
2017-10-11 22:29:51 -0700

    TransferInfo is not used

--- contracts/RegulatorService.sol
+++ contracts/RegulatorService.sol
@@ -1,13 +1,6 @@
 pragma solidity ^0.4.15;
 
 contract RegulatorService {
-  struct TransferInfo {
-    address token;
-    address from;
-    address to;
-    uint    amount;
-  }
-
   function get(address _address, uint256 _fact) constant returns (bool);
   function put(address _address, uint256 _fact, bool _value);
   function check(address _token, address _from, address _to, uint256 _amount) constant returns (bool);
